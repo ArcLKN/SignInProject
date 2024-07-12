c1c23f4e44046d786ffd473863bcfeb64cface44
ArcLKN <kalidolkn@gmail.com>
2024-07-12 14:15:57 +0400

    Added check to delete user route

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,5 +1,6 @@
 // /server/routes/UserRoutes.js
 const express = require("express");
+const { check } = require("express-validator");
 const authenticateToken = require("../middlewares/auth");
 const {
 	getUsers,
@@ -12,6 +13,17 @@ router.get("/users", authenticateToken, getUsers);
 //router.get("/users/:id", authenticateToken, getUser);
 router.post("/users", authenticateToken, addUser);
 //router.put("/users/:id", authenticateToken, updateUser);
-router.delete("/users/:id", authenticateToken, deleteOneUser);
+router.delete(
+	"/users/:id",
+	[
+		check("id")
+			.notEmpty()
+			.withMessage("User Id is required")
+			.isMongoId()
+			.withMessage("MongoId is required"),
+	],
+	authenticateToken,
+	deleteOneUser
+);
 
 module.exports = router;
