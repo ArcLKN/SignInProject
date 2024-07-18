68eaac62eb9a2484d4a7a3322086f173e4e14ec6
ArcLKN <kalidolkn@gmail.com>
2024-07-18 11:41:51 +0400

    Rework of the options + Added password as an option user's menu

--- server/controllers/manageUserController.js
+++ server/controllers/manageUserController.js
@@ -3,35 +3,34 @@ const UserModel = require("../models/User");
 
 async function sendPossibleManageOptions(req, res) {
 	try {
-		let options;
 		let userType = await UserModel.findOne({ _id: req.user._id }).select(
 			"userType"
 		);
+		let isOwner = req.user._id === req.params.id;
+		let options = {};
 		if (["Admin", "Super Admin"].includes(userType.userType)) {
-			options = {
-				edit: {
-					type: "PUT",
-					url: "/api/users/:id",
-					name: "Edit",
-				},
-				delete: {
-					type: "DELETE",
-					url: "/api/users/:id",
-					name: "Delete",
-				},
+			options.edit = {
+				type: "PUT",
+				url: "/api/users/:id",
+				name: "Edit",
 			};
-		} else {
-			if (req.params.id == req.user._id) {
-				options = {
-					edit: {
-						type: "PUT",
-						url: "/api/users/:id",
-						name: "Edit",
-					},
-				};
-			} else {
-				options = {};
-			}
+			options.delete = {
+				type: "DELETE",
+				url: "/api/users/:id",
+				name: "Delete",
+			};
+		}
+		if (isOwner) {
+			options.edit = {
+				type: "PUT",
+				url: "/api/users/:id",
+				name: "Edit",
+			};
+			options.password = {
+				type: "PUT",
+				url: "/api/users/:id/password",
+				name: "Reset Password",
+			};
 		}
 		return res.status(200).json({ msg: options, error: "Success" });
 	} catch (error) {
