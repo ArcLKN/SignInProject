95f353b0206c2e1f318e8e68600f2a351f1038bc
ArcLKN <kalidolkn@gmail.com>
2024-07-18 11:06:12 +0400

    Moved checkAdmin as a constant variable in usersPage

--- client/src/components/addUserButton.jsx
+++ client/src/components/addUserButton.jsx
@@ -3,50 +3,6 @@ import { Box, Flex, HStack, Button } from "@chakra-ui/react";
 import { colors } from "../styleVariables";
 
 const AddUserButton = ({ showAddUserModal }) => {
-	const [isAdmin, setIsAdmin] = useState(false);
-	const [loading, setLoading] = useState(true);
-
-	const token = localStorage.getItem("token");
-	if (!token) {
-		return <></>;
-	}
-
-	useEffect(() => {
-		const checkAdmin = async () => {
-			try {
-				const response = await window.fetch(
-					`http://localhost:3001/api/isAdmin/${token}`,
-					{
-						method: "GET",
-						headers: {
-							Authorization: `Bearer ${token}`,
-						},
-					}
-				);
-				if (!response.ok) {
-					throw new Error(`Error: ${response.statusText}`);
-				}
-				const json = await response.json();
-				if (json.msg) {
-					setIsAdmin(true);
-				}
-			} catch (error) {
-				console.error("There was an error!", error);
-			} finally {
-				setLoading(false);
-			}
-		};
-		checkAdmin();
-	}, [token]);
-
-	if (loading) {
-		return <></>;
-	}
-
-	if (!isAdmin) {
-		return <></>;
-	}
-
 	return (
 		<Box>
 			<Flex direction='row'>
