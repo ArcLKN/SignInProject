9bcc8c0d5b43dee3d22e99bae1db2d848d626aac
ArcLKN <kalidolkn@gmail.com>
2024-07-30 12:12:38 +0400

    Init new Project page

---
+++ client/src/api/ProjectsRoutes.jsx
@@ -0,0 +1,82 @@
+export async function createNewProject(data) {
+	console.log(data);
+	const token = localStorage.getItem("token");
+	if (!token) return { error: "No token found" };
+	try {
+		const response = await window.fetch(
+			`http://localhost:3001/api/projects`,
+			{
+				method: "PUT",
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+				body: data,
+			}
+		);
+		if (!response.ok) {
+			const errorData = await response.json();
+			throw new Error(
+				`${response.status} ${errorData.error || response.statusText}`
+			);
+		}
+		const result = await response.json();
+		return result;
+	} catch (error) {
+		console.error("There was an error!", error);
+		return { error: error.message };
+	}
+}
+
+export async function getUserProjects() {
+	const token = localStorage.getItem("token");
+	if (!token) return { error: "No token found" };
+	try {
+		const response = await window.fetch(
+			`http://localhost:3001/api/projects`,
+			{
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		);
+		if (!response.ok) {
+			const errorData = await response.json();
+			throw new Error(
+				`${response.status} ${errorData.error || response.statusText}`
+			);
+		}
+		const result = await response.json();
+		return result;
+	} catch (error) {
+		console.error("There was an error!", error);
+		return { error: error.message };
+	}
+}
+
+export async function getProjectFromId(id) {
+	const token = localStorage.getItem("token");
+	if (!token) return { error: "No token found" };
+	try {
+		const response = await window.fetch(
+			`http://localhost:3001/api/projects/${id}`,
+			{
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		);
+		if (!response.ok) {
+			const errorData = await response.json();
+			throw new Error(
+				`${response.status} ${errorData.error || response.statusText}`
+			);
+		}
+		const result = await response.json();
+		return result;
+	} catch (error) {
+		console.error("There was an error!", error);
+		return { error: error.message };
+	}
+}
