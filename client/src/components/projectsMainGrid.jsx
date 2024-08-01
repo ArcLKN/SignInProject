96341783343dc25c019fb45b25c383ff1ae0cef8
ArcLKN <kalidolkn@gmail.com>
2024-08-01 13:51:16 +0400

    Componentized the projects gallery part and removed useless stuff

---
+++ client/src/components/projectsMainGrid.jsx
@@ -0,0 +1,56 @@
+import { Flex, Box, Text, HStack, Image, SimpleGrid } from "@chakra-ui/react";
+import { colors } from "../styleVariables.jsx";
+import UsersEmptyState from "../components/usersEmptyState.jsx";
+
+export default function ProjectsMainGrid({ userProjects }) {
+	return (
+		<HStack spacing='24px'>
+			{Object.keys(userProjects).length > 0 ? (
+				<SimpleGrid
+					columns={{
+						base: 1,
+						sm: 2,
+						md: 3,
+						lg: 4,
+					}}
+					spacing={10}
+				>
+					{userProjects.map((project) => (
+						<Box
+							key={project._id}
+							border='1px solid'
+							borderColor={colors.vligrey}
+							rounded='5px'
+							w='300px'
+							h='210px'
+							p='3'
+							_hover={{
+								boxShadow: "lg",
+							}}
+							onClick={() =>
+								navigate(
+									`/projects/${project.title}-${project._id}`
+								)
+							}
+						>
+							<Flex alignItems='center' justify='center'>
+								<Image
+									src={`http://localhost:3001/images/${project.images[0]}`}
+									rounded='5px'
+									boxSize='170px'
+									w='300px'
+									objectFit='cover'
+								/>
+							</Flex>
+							<Text fontWeight='bold' noOfLines={1}>
+								{project.title}
+							</Text>
+						</Box>
+					))}
+				</SimpleGrid>
+			) : (
+				<UsersEmptyState />
+			)}
+		</HStack>
+	);
+}
--- client/src/pages/projectsPage.jsx
+++ client/src/pages/projectsPage.jsx
@@ -6,58 +6,21 @@ import {
 	Flex,
 	Box,
 	Text,
-	HStack,
-	Image,
-	SimpleGrid,
 	useBreakpointValue,
 } from "@chakra-ui/react";
-import { colors } from "../styleVariables.jsx";
 import { getUserProjects } from "../api/ProjectsRoutes.jsx";
 import NavBar from "../components/navBar.jsx";
 import CreateProjectModal from "../components/createProjectModal.jsx";
-import UsersEmptyState from "../components/usersEmptyState.jsx";
+import ProjectsMainGrid from "../components/projectsMainGrid.jsx";
 
 export default function Projects() {
 	const navigate = useNavigate();
-	const [userName, setUserName] = useState({
-		firstName: "Unknown",
-		lastName: "Unknown",
-	});
 	const [doCreateProjectModalIsOpen, setDoCreateProjectModalIsOpen] =
 		useState(false);
 	const [userProjects, setUserProjects] = useState([]);
 	//const [sortedProjects, setSortedProjects] = useState([]);
 
 	useEffect(() => {
-		const getUserName = async () => {
-			const token = localStorage.getItem("token");
-			if (!token) {
-				return;
-			}
-			try {
-				const response = await window.fetch(
-					`http://localhost:3001/api/getUserName/${token}`,
-					{
-						method: "GET",
-						headers: {
-							Authorization: `Bearer ${token}`,
-						},
-					}
-				);
-				if (!response.ok) {
-					throw new Error(`${response.statusText}`);
-				}
-				const json = await response.json();
-				if (json.msg) {
-					setUserName({
-						firstName: json.msg.firstName,
-						lastName: json.msg.lastName,
-					});
-				}
-			} catch (error) {
-				console.error("There was an error!", error);
-			}
-		};
 		const fetchUserProjects = async () => {
 			try {
 				const projects = await getUserProjects();
@@ -74,12 +37,7 @@ export default function Projects() {
 		};
 
 		fetchUserProjects();
-		getUserName();
 	}, []);
-
-	const projectsMap = userProjects.map((project) => {
-		<Text>{project.title}</Text>;
-	});
 
 	const paddingXValue = useBreakpointValue({ base: "2", md: "32" });
 	const paddingYValue = useBreakpointValue({ base: "2", md: "8" });
@@ -121,60 +79,7 @@ export default function Projects() {
 						</Box>
 						<Box mt='4'>
 							<Flex justify='center'>
-								<HStack spacing='24px'>
-									{Object.keys(userProjects).length > 0 ? (
-										<SimpleGrid
-											columns={{
-												base: 1,
-												sm: 2,
-												md: 3,
-												lg: 4,
-											}}
-											spacing={10}
-										>
-											{userProjects.map((project) => (
-												<Box
-													key={project._id}
-													border='1px solid'
-													borderColor={colors.vligrey}
-													rounded='5px'
-													w='300px'
-													h='210px'
-													p='3'
-													_hover={{
-														boxShadow: "lg",
-													}}
-													onClick={() =>
-														navigate(
-															`/projects/${project.title}-${project._id}`
-														)
-													}
-												>
-													<Flex
-														alignItems='center'
-														justify='center'
-													>
-														<Image
-															src={`http://localhost:3001/images/${project.images[0]}`}
-															rounded='5px'
-															boxSize='170px'
-															w='300px'
-															objectFit='cover'
-														/>
-													</Flex>
-													<Text
-														fontWeight='bold'
-														noOfLines={1}
-													>
-														{project.title}
-													</Text>
-												</Box>
-											))}
-										</SimpleGrid>
-									) : (
-										<UsersEmptyState />
-									)}
-								</HStack>
+								<ProjectsMainGrid userProjects={userProjects} />
 							</Flex>
 						</Box>
 					</Box>
