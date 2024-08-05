66ecaffe7bf6c6864472c3d0f1dbd66f33dc0f76
ArcLKN <kalidolkn@gmail.com>
2024-08-05 16:28:10 +0400

    Made a component of the individual images

--- client/src/components/projectsMainGrid.jsx
+++ client/src/components/projectsMainGrid.jsx
@@ -1,10 +1,8 @@
-import { Flex, Box, Text, HStack, Image, SimpleGrid } from "@chakra-ui/react";
-import { colors } from "../styleVariables.jsx";
+import { HStack, SimpleGrid } from "@chakra-ui/react";
 import UsersEmptyState from "../components/usersEmptyState.jsx";
-import { useNavigate } from "react-router-dom";
+import ProjectGridImage from "./projectGridImage.jsx";
 
-export default function ProjectsMainGrid({ userProjects }) {
-	const navigate = useNavigate();
+export default function ProjectsMainGrid({ userProjects, deleteProject }) {
 	return (
 		<HStack spacing='24px'>
 			{Object.keys(userProjects).length > 0 ? (
@@ -18,36 +16,11 @@ export default function ProjectsMainGrid({ userProjects }) {
 					spacing={10}
 				>
 					{userProjects.map((project) => (
-						<Box
+						<ProjectGridImage
 							key={project._id}
-							border='1px solid'
-							borderColor={colors.vligrey}
-							rounded='5px'
-							w='300px'
-							h='210px'
-							p='3'
-							_hover={{
-								boxShadow: "lg",
-							}}
-							onClick={() =>
-								navigate(
-									`/projects/${project.title}-${project._id}`
-								)
-							}
-						>
-							<Flex alignItems='center' justify='center'>
-								<Image
-									src={`http://localhost:3001/images/${project.images[0]}`}
-									rounded='5px'
-									boxSize='170px'
-									w='300px'
-									objectFit='cover'
-								/>
-							</Flex>
-							<Text fontWeight='bold' noOfLines={1}>
-								{project.title}
-							</Text>
-						</Box>
+							project={project}
+							deleteProject={deleteProject}
+						/>
 					))}
 				</SimpleGrid>
 			) : (
